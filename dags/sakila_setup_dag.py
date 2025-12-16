e514c74a102f00506fbf6cd268557b1cd0efb102
Timothy <mailcomruby@gmail.com>
2025-12-16 13:18:27 +0100

    Changing logic from local to remote

--- dags/sakila_setup_dag.py
+++ dags/sakila_setup_dag.py
@@ -1,10 +1,12 @@
 from airflow.sdk import dag
-from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.providers.standard.operators.bash import BashOperator
 from datetime import datetime
+from include.config import SAKILA_URL, EXTRACT_DIR, SAKILA_SCHEMA_FILE, SAKILA_DATA_FILE, SAKILA_CONN_ID
 
 default_args = {
     'owner': 'airflow',
 }
+
 
 @dag(
     default_args=default_args,
@@ -15,20 +17,43 @@ default_args = {
 )
 def sakila_setup_dag():
 
-    create_schema_task = SQLExecuteQueryOperator(
-        task_id='sakila_create_schema_task',
-        conn_id='mysql_sakila_conn_id',
-        sql="sql/sakila-schema.sql",
-        split_statements=True
+    db_env = {
+        'DB_HOST': f'{{{{ conn.{SAKILA_CONN_ID}.host }}}}',
+        'DB_PORT': f'{{{{ conn.{SAKILA_CONN_ID}.port }}}}',
+        'DB_USER': f'{{{{ conn.{SAKILA_CONN_ID}.login }}}}',
+        'MYSQL_PWD': f'{{{{ conn.{SAKILA_CONN_ID}.password }}}}'
+    }
+
+    download_files_task = BashOperator(
+        task_id='download_files',
+        bash_command=f"""
+            curl -L "{SAKILA_URL}" -o /tmp/sakila-db.tar.gz && \
+            tar -xzf /tmp/sakila-db.tar.gz -C {EXTRACT_DIR}
+            """,
+    )
+    create_schema_task = BashOperator(
+        task_id='create_schema',
+        bash_command=f"""
+            mysql -h $DB_HOST \
+                  -P $DB_PORT \
+                  -u $DB_USER \
+                  -e "source {SAKILA_SCHEMA_FILE}"
+            """,
+        env=db_env
     )
 
-    insert_data_task = SQLExecuteQueryOperator(
-        task_id='sakila_insert_data_task',
-        conn_id='mysql_sakila_conn_id',
-        sql="sql/sakila-data.sql",
-        split_statements=True
+    insert_data_task = BashOperator(
+        task_id='insert_data',
+        bash_command=f"""
+                mysql -h $DB_HOST \
+                  -P $DB_PORT \
+                  -u $DB_USER \
+                  -e "source {SAKILA_DATA_FILE}"
+            """,
+        env=db_env,
     )
 
-    create_schema_task >> insert_data_task
+    download_files_task >> create_schema_task >> insert_data_task
+
 
 sakila_setup_dag()
